b42e469704a709f2a6625b0b32ef851ba41022d5
ShawnBorn-Newton <shawn.newton3@gmail.com>
2017-07-28 10:35:58 -0400

    added models

---
+++ PokeballReview/Models/Categories.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PokeballReview.Models
+{
+    public class Categories
+    {
+        [Key]
+        int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+
+        ICollection<Reviews> Reviews { get; set; }
+    }
+}
